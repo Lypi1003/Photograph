a3d345a025016ae9ff8e3a27a286f3d17c5855f7
Lypi1003 <ldkk@mail.bg>
2022-08-08 15:52:01 +0300

    Add catalog html

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import { Header } from "./components/Header";
 import './App.css';
 import { Footer } from "./components/Footer";
+import { Catalog } from "./components/Catalog";
 
 
 function App() {
   return (
     <div >
-      
-    <Header />
-    <main>
-      
-    </main>
-    
-    <Footer/>
+
+      <Header />
+
+      <main>
+
+          <Catalog/>
+
+      </main>
+
+      <Footer />
     </div>
   );
 }
